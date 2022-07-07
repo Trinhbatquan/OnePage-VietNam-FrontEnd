03a3604a6564c5eaa514527eb3aad8080670cee7
Trình <trinhbatquan2001@gmail.com>
2022-07-07 21:52:16 +0700

    upload source code

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.image-slider').slick({
+        slidesToShow: 1,
+        infinity: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        draggable: true,
+    });
+
+
+
+});
